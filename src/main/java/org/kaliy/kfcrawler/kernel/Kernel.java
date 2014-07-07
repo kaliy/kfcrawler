03868ccbe3afd39bc23590c9e68a36260b5462b8
Arthur Kalimullin <kalimullin@gmail.com>
2014-07-07 18:38:04 +0400

    Initial commit

---
+++ src/main/java/org/kaliy/kfcrawler/kernel/Kernel.java
@@ -0,0 +1,14 @@
+package org.kaliy.kfcrawler.kernel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Kernel {
+
+    private final static Logger logger = LoggerFactory.getLogger(Kernel.class);
+
+    public static void main(String... args) {
+        logger.info("KFCrawler is starting");
+    }
+
+}
