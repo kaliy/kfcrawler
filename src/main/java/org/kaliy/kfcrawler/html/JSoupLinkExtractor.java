ffc20eff8a6de008322b00125cfa4024f5985ec9
Arthur Kalimullin <kalimullin@gmail.com>
2014-07-15 15:04:51 +0400

    Escaping parameters in URL

--- src/main/java/org/kaliy/kfcrawler/html/JSoupLinkExtractor.java
+++ src/main/java/org/kaliy/kfcrawler/html/JSoupLinkExtractor.java
@@ -1,13 +1,19 @@
 package org.kaliy.kfcrawler.html;
 
+import com.google.common.base.Strings;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Entities;
 import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,14 +23,21 @@ public class JSoupLinkExtractor implements LinkExtractor {
 
     @Override
     public Set<URL> extractFromHtml(String html, URL baseUrl) {
-        Elements linkElements = Jsoup.parse(html, baseUrl.toString()).select("a[href]");
+        Document document =  Jsoup.parse(html, baseUrl.toString());
+        document.outputSettings().escapeMode(Entities.EscapeMode.extended);
+        Elements linkElements = document.select("a[href]");
         Set<URL> urls = new HashSet<>();
         for(Element element: linkElements) {
-            String url = element.attr("abs:href");
-            try {
-                urls.add(new URL(url));
-            } catch (MalformedURLException e) {
-                logger.warn("Malformed URL: \"{}\", skipping", url);
+            String urlString = element.attr("abs:href");
+            if (!Strings.isNullOrEmpty(urlString)) {
+                try {
+                    URL url = new URL(urlString);
+                    URI uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), null);
+                    url = uri.toURL();
+                    urls.add(url);
+                } catch (Exception e) {
+                    logger.warn("Malformed URL: \"{}\", skipping", urlString);
+                }
             }
         }
         return urls;
